6346cf83f61d1fd2d2fc1f7a5faafe75fccc8649
Alec Friedhoff <afriedhoff@gmail.com>
2017-09-22 16:54:30 -0400

    initial work here

--- build/data/eda0.R
+++ build/data/eda0.R
@@ -1,1 +1,8 @@
+library(readxl)
+library(dplyr)
+
 #exploratory data analysis for water
+dat <- read_xlsx("/home/alec/Projects/Brookings/water/build/data/final/FINAL_Metro_FullBreakdown_85_10_Alec.xlsx", sheet="2010_Metro_FullBreakdown")
+
+#need to figure out defs
+dat2 <- dat %>% mutate(tot=`PS-Wtotl`+`DO-WFrTo`+`DO-PSDel`+`IN-Wtotl`,`IR-WFrTo`,`LI-WFrTo`,`AQ-WTotl`,`MI-Wtotl`,`PT-Wtotl`)
